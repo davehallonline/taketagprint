cea9039efd09f9697aba4d8fd460865c16c35d17
davehallonline <davehall98@gmail.com>
2021-05-03 23:20:11 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -42,17 +42,17 @@ function(accessToken, refreshToken, profile, done){
 //app.configure(function() {
     app.set('views', __dirname + '/views');
     app.set('view engine', 'ejs');
-    // app.use(express.logger());
-    // app.use(express.cookieParser());
-    // app.use(express.bodyParser());
-    // app.use(express.methodOverride());
-    // app.use(express.session({ secret: 'keyboard cat' }));
-    // // Initialize Passport!  Also use passport.session() middleware, to support
-    // // persistent login sessions (recommended).
+    app.use(express.logger());
+    app.use(express.cookieParser());
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+    app.use(express.session({ secret: 'keyboard cat' }));
+    // Initialize Passport!  Also use passport.session() middleware, to support
+    // persistent login sessions (recommended).
      app.use(passport.initialize());
      app.use(passport.session());
-    // app.use(app.router);
-    // app.use(express.static(__dirname + '/public'));
+     app.use(app.router);
+     app.use(express.static(__dirname + '/public'));
   //});
 
 
